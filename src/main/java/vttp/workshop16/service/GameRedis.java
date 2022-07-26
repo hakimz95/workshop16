1ad235f0cd5c8a88a01ec6f8b1130f392ccba315
Khaizad52 <1khairulh@gmail.com>
2022-07-26 16:47:41 +0800

    5

--- src/main/java/vttp/workshop16/service/GameRedis.java
+++ src/main/java/vttp/workshop16/service/GameRedis.java
@@ -4,9 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
 
 import vttp.workshop16.model.BoardService;
 
+@Service
 public class GameRedis implements GameRepo {
     private static final Logger logger = LoggerFactory.getLogger(GameRedis.class);
 
