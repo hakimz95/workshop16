547a077394c4aa76cd63271ef6781781077a9267
FrederickCWX <fcweixun@gmail.com>
2022-07-26 14:04:34 +0800

    test

--- src/main/java/vttp/workshop16/Workshop16Application.java
+++ src/main/java/vttp/workshop16/Workshop16Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Workshop16Application {
-
+//test
 	public static void main(String[] args) {
 		SpringApplication.run(Workshop16Application.class, args);
 	}
